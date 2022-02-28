2643191df94de7b12c0fa9624cf754d5112bc0e8
David L <open-source@davidcarbon.dev>
2022-02-28 11:34:46 -0800

    Add new entry for Json Frequency Cache and XML Embeded
    Added:
    - New Save Entry for Json Frequency Update
    - Embeded XML File

---
+++ SBRW.Launcher.Core.Extra/File_/Extract_Resource.cs
@@ -0,0 +1,76 @@
+﻿using SBRW.Launcher.Core.Extension.Logging_;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using System.Text;
+
+namespace SBRW.Launcher.Core.Extra.File_
+{
+    internal class Extract_Resource
+    {
+        internal static byte[] AsByte(String File_Name)
+        {
+            if (string.IsNullOrWhiteSpace(File_Name))
+            {
+                return null;
+            }
+            else
+            {
+                try
+                {
+                    Assembly TheRun = Assembly.GetExecutingAssembly();
+                    using (Stream LiveStream = TheRun.GetManifestResourceStream(File_Name))
+                    {
+                        if (LiveStream == null) { return null; }
+                        else
+                        {
+                            byte[] ba = new byte[LiveStream.Length];
+                            LiveStream.Read(ba, 0, ba.Length);
+                            return ba;
+                        }
+                    }
+                }
+                catch (Exception Error)
+                {
+                    Log_Detail.OpenLog("Extract Resource AsByte", null, Error, null, true);
+                    return null;
+                }
+            }
+        }
+
+        internal static string AsString(string File_Name)
+        {
+            if (string.IsNullOrWhiteSpace(File_Name))
+            {
+                return string.Empty;
+            }
+            else
+            {
+                try
+                {
+                    Assembly TheRun = Assembly.GetExecutingAssembly();
+                    using (Stream LiveStream = TheRun.GetManifestResourceStream(File_Name))
+                    {
+                        if (LiveStream == null) 
+                        { 
+                            return string.Empty; 
+                        }
+                        else
+                        {
+                            using (StreamReader StreamViewer = new StreamReader(LiveStream))
+                            {
+                                return StreamViewer.ReadToEnd();
+                            }
+                        }
+                    }
+                }
+                catch (Exception Error)
+                {
+                    Log_Detail.OpenLog("Extract Resource AsString", null, Error, null, true);
+                    return string.Empty;
+                }
+            }
+        }
+    }
+}
